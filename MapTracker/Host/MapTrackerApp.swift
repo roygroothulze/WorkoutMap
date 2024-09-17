00fc92e5864cdd8268b113beac440188a2cce972
Roy Groot Hulze <info@roygroothulze.nl>
2024-09-17 21:38:42 +0200

    Added: TelemetryDeck

--- MapTracker/Host/MapTrackerApp.swift
+++ MapTracker/Host/MapTrackerApp.swift
@@ -7,24 +7,34 @@
 
 import SwiftUI
 import SwiftData
+import TelemetryDeck
 
 @main
 struct MapTrackerApp: App {
     var sharedModelContainer: ModelContainer = {
-            let schema = Schema([
-                Route.self,
-                RoutePart.self,
-                RoutePinLocation.self,
-                CoordinateData.self,
-            ])
-            let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let schema = Schema([
+            Route.self,
+            RoutePart.self,
+            RoutePinLocation.self,
+            CoordinateData.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-            do {
-                return try ModelContainer(for: schema, configurations: [modelConfiguration])
-            } catch {
-                fatalError("Could not create ModelContainer: \(error)")
-            }
-        }()
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    init() {
+        let appId = (Bundle.main.object(forInfoDictionaryKey: "TELEMENTRYDECK_APP_ID") as? String) ?? ProcessInfo.processInfo.environment["TELEMENTRYDECK_APP_ID"]
+        
+        if let appId {
+            TelemetryDeck.initialize(config: .init(appID: appId))
+        }
+        
+    }
     
     var body: some Scene {
         WindowGroup {
